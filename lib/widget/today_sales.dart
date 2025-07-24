bf131dfc7a614debecdb2c1e64d443289c8fa66e
Rajeshjoesph <rajeshkannan1999.rk@gmail.com>
2025-07-24 21:51:35 +0530

    Enhance TodaySales widget layout with detailed sales cards and improved styling for better data presentation

--- lib/widget/today_sales.dart
+++ lib/widget/today_sales.dart
@@ -5,17 +5,198 @@ class TodaySales extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       padding: EdgeInsets.all(16.0),
-      height: 40,
+      height: 140,
       decoration: BoxDecoration(
         color: Color(0xFF1A1C23), // Background color
         // borderRadius: BorderRadius.circular(12),
       ),
       child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-          Text(
-            'Today\'s Sales',
-            style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+          Row(
+            children: [
+              Container(
+                  width: 800,
+                  height: 400,
+                  padding: EdgeInsets.all(18.0),
+                  decoration: BoxDecoration(
+                    color: Color(0xFF2A2B37), // Card background color
+                    borderRadius: BorderRadius.circular(8.0),
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Today\'s Sales',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Text(
+                        'Sales Summary',
+                        style: TextStyle(
+                          color: Colors.white70,
+                          fontSize: 14,
+                        ),
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        // padding: EdgeInsets.only(top: 10),
+                        children: [
+                          Card(
+                              color: Color(0xFF1A1C23),
+                              child: SizedBox(
+                                  width: 150,
+                                  height: 150,
+                                  child: Padding(
+                                    padding: EdgeInsets.all(10),
+                                    child: Column(
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.start,
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.spaceEvenly,
+                                        // padding: EdgeInsets.all(10),
+                                        children: [
+                                          Icon(Icons.analytics_outlined,
+                                              color: Colors.yellow, size: 40),
+                                          Text(
+                                            '\$ 5K',
+                                            style: TextStyle(
+                                                color: Colors.white,
+                                                fontSize: 20),
+                                          ),
+                                          Text(
+                                            'Total Sales',
+                                            style: TextStyle(
+                                                color: Colors.white70,
+                                                fontSize: 14),
+                                          ),
+                                          Text(
+                                            '+10% from yesterday',
+                                            style: TextStyle(
+                                                color: Colors.yellowAccent,
+                                                fontSize: 10),
+                                          ),
+                                        ]),
+                                  ))),
+                          Card(
+                              color: Color(0xFF1A1C23),
+                              child: SizedBox(
+                                  width: 150,
+                                  height: 150,
+                                  child: Padding(
+                                    padding: EdgeInsets.all(10),
+                                    child: Column(
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.start,
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.spaceEvenly,
+                                        // padding: EdgeInsets.all(10),
+                                        children: [
+                                          Icon(Icons.not_accessible_outlined,
+                                              color: Colors.yellow, size: 40),
+                                          Text(
+                                            '\$ 5K',
+                                            style: TextStyle(
+                                                color: Colors.white,
+                                                fontSize: 20),
+                                          ),
+                                          Text(
+                                            'Total Sales',
+                                            style: TextStyle(
+                                                color: Colors.white70,
+                                                fontSize: 14),
+                                          ),
+                                          Text(
+                                            '+10% from yesterday',
+                                            style: TextStyle(
+                                                color: Colors.yellowAccent,
+                                                fontSize: 10),
+                                          ),
+                                        ]),
+                                  ))),
+                          Card(
+                              color: Color(0xFF1A1C23),
+                              child: SizedBox(
+                                  width: 150,
+                                  height: 150,
+                                  child: Padding(
+                                    padding: EdgeInsets.all(10),
+                                    child: Column(
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.start,
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.spaceEvenly,
+                                        // padding: EdgeInsets.all(10),
+                                        children: [
+                                          Icon(Icons.analytics_outlined,
+                                              color: Colors.yellow, size: 40),
+                                          Text(
+                                            '\$ 5K',
+                                            style: TextStyle(
+                                                color: Colors.white,
+                                                fontSize: 20),
+                                          ),
+                                          Text(
+                                            'Total Sales',
+                                            style: TextStyle(
+                                                color: Colors.white70,
+                                                fontSize: 14),
+                                          ),
+                                          Text(
+                                            '+10% from yesterday',
+                                            style: TextStyle(
+                                                color: Colors.yellowAccent,
+                                                fontSize: 10),
+                                          ),
+                                        ]),
+                                  ))),
+                          Card(
+                              color: Color(0xFF1A1C23),
+                              child: SizedBox(
+                                  width: 150,
+                                  height: 150,
+                                  child: Padding(
+                                    padding: EdgeInsets.all(10),
+                                    child: Column(
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.start,
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.spaceEvenly,
+                                        // padding: EdgeInsets.all(10),
+                                        children: [
+                                          Icon(Icons.analytics_outlined,
+                                              color: Colors.yellow, size: 40),
+                                          Text(
+                                            '\$ 5K',
+                                            style: TextStyle(
+                                                color: Colors.white,
+                                                fontSize: 20),
+                                          ),
+                                          Text(
+                                            'Total Sales',
+                                            style: TextStyle(
+                                                color: Colors.white70,
+                                                fontSize: 14),
+                                          ),
+                                          Text(
+                                            '+10% from yesterday',
+                                            style: TextStyle(
+                                                color: Colors.yellowAccent,
+                                                fontSize: 10),
+                                          ),
+                                        ]),
+                                  ))),
+                        ],
+                      ),
+                    ],
+                  )),
+            ],
           ),
+
           SizedBox(height: 16),
           // Placeholder for sales data
           Expanded(
